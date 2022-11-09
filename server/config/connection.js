2dc35f9964bdf3be7918fff15bfd6ddd76939156
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-11-09 11:33:54 -0500

    initial server setup done

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://0.0.0.0/deepen", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+});
