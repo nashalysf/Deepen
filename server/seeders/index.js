c2bd894875be6c837482cfecb73b93d489c11022
Leocrydis <82065055+Leocrydis@users.noreply.github.com>
2022-11-10 08:01:20 -0500

    set up basic data for seeds

--- server/seeders/index.js
+++ server/seeders/index.js
@@ -1,15 +1,19 @@
 const db = require('../config/connection');
-const {} = require('../models');
+const { User, Post } = require('../models');
 
-//once models are done insert routes for .json when made
+const userData = require('./userData.json');
+const postData = require('./userData.json');
 
 //
 db.once('open', async () => {
     //clean database
-    await({});
+    await User.deleteMany({});
+    await Post.deleteMany({});
 
     //bulk create each model
-    
+    const Users = await User.insertMany(userData);
+    const Posts = await Post.insertMany(postData);
+
     console.log('test')
     process.exit(0);
 });
