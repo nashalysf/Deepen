391b8a535385385fa01d27281586732df4aec804
nashalysf <nashalysf@gmail.com>
2022-11-14 19:43:39 -0400

    css layout

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import logo from '../../images/logo-black.png';
+import logo from '../../images/logo-white.png';
+import burgerBtn from '../../images/burgerBtn.PNG'
 const Header = () => {
   return (
-    <header className="bg-light mb-4 py-2 flex-row align-center">
+    <header className=" mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
       <img
             src={logo}
             className="logo"
-            style={{ width: "10%" }}
+            style={{ width: "4%" }}
             alt="logopic"
-          /><h1 className='logo'>Deepen</h1>
+          />
+          <img
+            src={burgerBtn}
+            className="Burgerbtn"
+            style={{ width: "3%" }}
+            alt="menu button"
+          />
       </div>
     </header>
   );
