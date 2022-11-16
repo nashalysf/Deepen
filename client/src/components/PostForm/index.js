b11178a88e67febf40216dc8fa29aee0ab781528
Richard <rachistoteles@gmail.com>
2022-11-15 22:39:27 -0400

    Fixed postForm duplicate error count

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -64,12 +64,6 @@ const PostForm = () => {
 
   return (
     <div>
-      <p
-        className={`m-0 ${characterCount === 280 || error ? "text-error" : ""}`}
-      >
-        Character Count: {characterCount}/280
-        {error && <span className="ml-2">Something went wrong...</span>}
-      </p>
       <form
         className="flex-row justify-center justify-space-between-md align-stretch"
         onSubmit={handleFormSubmit}
