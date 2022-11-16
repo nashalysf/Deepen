e54514ffcc3d7533b213f3926b02cf4ad1e33277
nashalysf <nashalysf@gmail.com>
2022-11-16 15:04:29 -0400

    images path fixed

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,7 +11,7 @@ import Register from "../../pages/Register";
 import Login from "../../pages/Login";
 import logo from "../../images/logo-white.svg";
 import Auth from "../../utils/auth";
-import burgerBtn from "../../images/burgerBtn.PNG";
+import burgerBtn from "../../images/png/burgerBtn.PNG";
 import { Navigate } from "react-router-dom";
 
 
