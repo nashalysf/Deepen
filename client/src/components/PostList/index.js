d0c65628cc23817c66cc0beb4906afbfcad5bade
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-11-15 21:11:31 -0500

    Home pulls all users posts

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,78 +1,36 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-
 import { Link } from 'react-router-dom';
-import { QUERY_POSTS } from '../../utils/queries'
-import Auth from '../../utils/auth'
-import { useQuery } from '@apollo/client';
 
-const PostList = (props) => {
-  const { id: postsId } = useParams();
-
-  const { loading, data } = useQuery(QUERY_POSTS, {
-    variables: { id: postsId },
-  });
-
-  const post = data?.post || {};
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+const PostList = ({ posts, title }) => {
+  if (posts != null && !posts.length) {
+    return <h3 className='noPost'>No Posts Yet</h3>;
+}
 
   return (
     <div>
-      <div className="card mb-3">
-        <p className="card-header">
-          <span style={{ fontWeight: 700 }} className="text-light">
-            {post.username}
-          </span>{' '}
-          testing {post.createdAt}
-        </p>
-        <div className="card-body">
-          <p>{post.decription}</p>
-        </div>
-      </div>
-
-      {post.decription > 0 && (
-        <Likecount post={post.likecount} />
-      )}
-
-      {Auth.loggedIn() && <Likecount post={post._id} />}
+      <h3>{title}</h3>
+      {posts &&
+        posts.map(post => (
+          <div key={post._id} className="postCard mb-3">
+            <p className="postCard-header post-header">
+              <Link to ={`profile/${post.username}`}>
+              {post.username}
+              </Link>{' '}
+              post on {post.createdAt}
+            </p>
+            <div className="postCard-body post-body">
+              <Link to={`/post/${post._id}`}>
+              <p>{post.title}</p>
+              <p className="mb-0">
+                likes: {post.likeCount} || Click to{' '}
+                {post.commentCount ? 'see' : 'start'} the discussion!
+              </p>
+            </Link>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
-
-
-// const postList = ({ posts, title }) => {
-//   if (posts != null && !posts.length) {
-//     return <h3 className='noPost'>No Posts Yet</h3>;
-// }
-
-//   return (
-//     <div>
-//       <h3>{title}</h3>
-//       {posts &&
-//         posts.map(post => (
-//           <div key={post._id} className="postCard mb-3">
-//             <p className="postCard-header post-header">
-//               <Link to ={`profile/${post.username}`}>
-//               {post.username}
-//               </Link>{' '}
-//               post on {post.createdAt}
-//             </p>
-//             <div className="postCard-body post-body">
-//               <Link to={`/post/${post._id}`}>
-//               <p>{post.title}</p>
-//               <p className="mb-0">
-//                 likes: {post.likeCount} || Click to{' '}
-//                 {post.commentCount ? 'see' : 'start'} the discussion!
-//               </p>
-//             </Link>
-//             </div>
-//           </div>
-//         ))}
-//     </div>
-//   );
-// };
 
 export default PostList;
