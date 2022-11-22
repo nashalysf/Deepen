4badec7d1a6365c8263cecfbd97210f61ac5a531
Richard <rachistoteles@gmail.com>
2022-11-22 11:36:37 -0400

    Post List Upgraded

--- client/src/components/Home/PostList.js
+++ client/src/components/Home/PostList.js
@@ -1,34 +1,85 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { CardHeader } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import { red } from "@mui/material/colors";
 
 const PostList = ({ posts, title }) => {
   if (!posts.length) {
-    return <h3 className='noPost'>No Posts Yet</h3>;
-}
+    return <h3 className="noPost">No Posts Yet</h3>;
+  }
 
+  const theme = createTheme();
   return (
     <div>
       <h3>{title}</h3>
-      {posts &&
-        posts.map(post => (
-          <div key={post._id} className="postCard mb-3">
-            <p className="postCard-header post-header">
-              <Link to ={`profile/${post.username}`}>
-              {post.username}
-              </Link>{' '}
-              post on {post.createdAt}
-            </p>
-            <div className="postCard-body post-body">
-              <Link to={`/post/${post._id}`}>
-              <p>{post.title}</p>
-              <p className="mb-0">
-                likes: {post.likeCount} || Click to{' '}
-                {post.commentCount ? 'see' : 'start'} the discussion!
-              </p>
-            </Link>
-            </div>
-          </div>
-        ))}
+      <div>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {posts &&
+              posts.map((post) => (
+                <Grid item key={post._id} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <CardHeader
+                      avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                          R
+                        </Avatar>
+                      }
+                      action={<IconButton aria-label="settings"></IconButton>}
+                      title={post.title}
+                      subheader={post.createdAt}
+                    />
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: "10%",
+                      }}
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {post._id}
+                      </Typography>
+                      <Typography>{post.description}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Link href={`/post/${post._id}`} underline="hover" >
+                        View Post
+                      </Link>
+                      {/* <Button size="small">View</Button>
+                      <Button size="small">Edit</Button> */}
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
+        </Container>
+      </div>
     </div>
   );
 };
