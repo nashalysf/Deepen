0dd37923db5f142d8573433d9ac7980f250097e5
Richard <rachistoteles@gmail.com>
2022-11-27 16:33:49 -0400

    new way

--- client/src/components/Buttons/CollabButton.js
+++ client/src/components/Buttons/CollabButton.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const CollabButton = (userIsCreator) => {
-  let [userBoolean, setUserBoolean] = useState(userIsCreator);
+  const [userBoolean, setUserBoolean] = useState(false);
   console.log(userIsCreator);
   const [button1] = useState(
     <button className="buttonAccept1">Accepted</button>
@@ -10,14 +10,22 @@ const CollabButton = (userIsCreator) => {
   const [button2] = useState(
     <button className="buttonAccept2">Pending</button>
   );
-
-  return (
-    <div>
-      <button className="btn" onClick={() => setUserBoolean(!userBoolean)}>
-        {userBoolean ? button1 : button2}
-      </button>
-    </div>
-  );
+  function IsCreator(props) {
+    return <div>
+    <button className="btn" onClick={() => setUserBoolean(!userBoolean)}>
+      {userBoolean ? button1 : button2}
+    </button>
+  </div>;
+  }
+  function NotCreator(props) {
+    return  <div>{userBoolean ? button1 : button2}</div>;
+  }
+  if (userIsCreator===true) {
+    return <NotCreator />;
+  }else{
+    return <IsCreator />;
+    }
 };
+ 
 
 export default CollabButton;
